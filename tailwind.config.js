235e0dbafd9f3f0fe503cf81cc44d1720e00ccdd
Yann Kamche <yannkamche@gmail.com>
2023-08-20 06:32:48 +0100

    Comleted the features section and fixed the color in the cofig file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       colors: {
         "color-primary": "#01051e", //prominent color: rgba(226, 138, 152, 0.937)
         "color-primary-light": "#020726",
-        "color-primary-dark": "010417",
+        "color-primary-dark": "#010417",
         "color-secondary": "#ff7d3b",
         "color-gray": "#333",
         "color-white": "#fff",
